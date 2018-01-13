3bcf4103f6dc5b53fbf7e501a67cd5971445c7bc
jnprogrammer <joshuanparker@gmail.com>
2018-01-12 21:21:21 -0500

    Completed webscrapper
    This programm correctly scrapes information and displays them in
    console. Doesn't have a database.

--- WebScrapper-EbayScrapper-v1/WebScrapper-EbayScrapper-v1/Program.cs
+++ WebScrapper-EbayScrapper-v1/WebScrapper-EbayScrapper-v1/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using HtmlAgilityPack;
+using System.Text.RegularExpressions;
 
 namespace WebScrapper_EbayScrapper_v1
 {
@@ -19,7 +20,7 @@ namespace WebScrapper_EbayScrapper_v1
 
         private static async void GetHtmlAsync()
         {
-            var url = "https://www.ebay.com/sch/i.html?_nkw=xbox+one&_in_kw=1&_ex_kw=&_sacat=0&LH_Complete=1&_udlo=&_udhi=&_samilow=&_samihi=&_sargn=-1%26saslc%3D1&_salic=1&_sop=12&_dmd=1&_ipg=50";
+            var url = "https://www.ebay.com/sch/i.html?_nkw=xbox+one&_in_kw=1&_ex_kw=&_sacat=0&LH_Complete=1&_udlo=&_udhi=&_samilow=&_samihi=&_sargn=-1%26saslc%3D1&_salic=1&_sop=12&_dmd=1&_ipg=200";
             var httpClient = new HttpClient();
             var html = await httpClient.GetStringAsync(url);
 
@@ -36,13 +37,37 @@ namespace WebScrapper_EbayScrapper_v1
                                                   .Where(node => node.GetAttributeValue("id", "")
                                                   .Contains("item"))
                                                   .ToList();
-
+            
             foreach(var productListItem in productListItems)
             {
-                Console.WriteLine(productListItem.GetAttributeValue("listingid","empty"));
+                var productTitles = productListItem.Descendants("h3")
+                                                   .Where(node => node.GetAttributeValue("class", "")
+                                                   .Equals("lvtitle"))
+                                                   .FirstOrDefault()
+                                                   .InnerText.Trim('\r', '\n', '\t');
+                //lvprice prc
+                var productPrices = Regex.Match( productListItem.Descendants("li")
+                                                                .Where(node => node.GetAttributeValue("class", "")
+                                                                .Equals("lvprice prc"))
+                                                                .FirstOrDefault()
+                                                                .InnerText.Trim('\r', '\n', '\t'),@"\d+.\d+");
+                //lvformat
+                var productBuyFormat = productListItem.Descendants("li")
+                                                                .Where(node => node.GetAttributeValue("class", "")
+                                                                .Equals("lvformat"))
+                                                                .FirstOrDefault()
+                                                                .InnerText.Trim('\r', '\n', '\t');
 
-                Console.WriteLine(productListItem.Descendants("h3").Where(node => node.GetAttributeValue("class","")
-                                                                   .Equals("lvtitle")).FirstOrDefault().InnerText.Trim('\r','\n','\t') + "\n");
+                var productUrl = productListItem.Descendants("a")
+                                                .FirstOrDefault()
+                                                .GetAttributeValue("href", "");
+
+                Console.WriteLine(productListItem.GetAttributeValue("listingid",""));
+
+                Console.WriteLine( productTitles );
+                Console.WriteLine( productBuyFormat );
+                Console.WriteLine( productPrices );
+                Console.WriteLine( productUrl + "\n");
             }
 
             Console.WriteLine();
